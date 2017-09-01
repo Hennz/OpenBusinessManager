3bbe9e53270ddd30c2c5ea1eb0dc7e1ab2c88544
Jared York <yorkwebdesign@gmail.com>
2017-08-31 23:01:37 -0400

    v1.0 - Project basics written.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta lang="en-us" />
+    <title>Business Manager | Dashboard</title>
+    <!-- CSS Reset link -->
+    <link rel="stylesheet" type="text/css" href="css/reset.css" />
+
+    <!-- App CSS links -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+  </head>
+
+  <body>
+    <div class="wrapper">
+      <header class="header-main">
+        <h2 class="heading-m">Business Manager</h2>
+      </header>
+
+      <div class="container-app-padding">
+        
+      </div>
+    </div>
+  </body>
+</html>
